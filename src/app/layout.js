cd9d53547daebf95968bbe5bc561e91a3859212e
Vikneshoftheleaf <114404122+Vikneshoftheleaf@users.noreply.github.com>
2023-12-22 21:51:48 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,22 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-<<<<<<< HEAD
         <AuthContextProvider>
             <main>{children}</main>
         <BottomNav />
-=======
-      <AuthContextProvider>
-      <MaxWidthWrapper>
-    
-          <div className="flex">
-            <SideNav />
-            <main className="flex-1">{children}</main>
-          </div>
-    
-        </MaxWidthWrapper>
-        <BottomNav/>
->>>>>>> 96771c02b4bb0bd25a5cf5858cc482ff1a883496
         </AuthContextProvider>
       </body>
     </html>

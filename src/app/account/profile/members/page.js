db4faa1cc0a9ac5e87daa4fc8410e8fb2e4d35d3
vikneshoftheleaf <vikneshoftheleaf@gmail.com>
2024-01-03 15:53:12 +0530

    real time update

--- src/app/account/profile/members/page.js
+++ src/app/account/profile/members/page.js
@@ -3,12 +3,6 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 export default function Members() {
-    /*const [membertype, setMemberType] = useState('follower')
-    const searchparam = useSearchParams();
-    const type = searchparam.get('type');
-    useEffect(()=>{
-        setMemberType(type)          
-    },[])*/
     return (
         <Tabs defaultValue='follower' className="w-full">
             <TabsList className="w-full flex justify-center">
